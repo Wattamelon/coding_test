30908d6952ccbb975a0059a528b75523d176011c
seongon Moon <seongon1010@gmail.com>
2026-01-07 19:05:21 +0900

    1436 solve

---
+++ boj/Silver5/1436_영화감독 숌(BruteForce)/solve.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+num = 665
+
+if n == 0 :
+    print(0)
+else:
+    while n>0:
+        num += 1
+        if "666" in str(num) :
+            n -= 1
+
+print(num)
---
+++ boj/Silver5/1543_문서 검색/other_solution.py
@@ -0,0 +1,5 @@
+a=input()
+b=input()
+
+a=a.split(b)
+print(len(a)-1)
