1d04ce6e690adeb21ca67ab45ca24983f4906b3e
Wattamelon <seongon1010@gmail.com>
2026-02-14 12:07:47 +0900

    sql

--- test.py
+++ test.py
@@ -1,49 +1,53 @@
 import sys
 from collections import deque
+
 input = sys.stdin.readline
 
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
+n , m , k , x = map(int,input().split())
 
+cities = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
 
-a , b = map(int,input().split())
-graph = []
-visited = []
-for _ in range(a):
-    graph.append(list(map(int,input().strip())))
-    visited.append([False]*b)
+for _ in range(m):
+    idx , num = map(int,input().split())
+    cities[idx].append(num)
 
-r , c = 0 , 0 # row , column
-
-queue = deque([(r,c)])
-visited[r][c] = True
-
+queue = deque([x])
 while queue:
-    r,c = queue.popleft()
-    for i in range(4):
-        x = r + dx[i]
-        y = c + dy[i]
-        if 0 <= x < a and 0 <= y < b:
-            if graph[x][y] == 0:
-                continue
-            else:
-                if graph[x][y] == 1 and not visited[x][y]:
-                    continue
-                else:
-                    graph[x][y] = graph[r][c] + 1
-                    queue.append((x,y))
-                    visited[x][y] = True
+    v = queue.popleft()
 
-print(graph[a-1][b-1])
-print(graph)
+    for i in cities[v]:
+        if visited[i] == 0:
+            queue.append(i)
+            visited[i] = visited[v] + 1
+
+out = []
+for idx , num in enumerate(visited):
+    if num == k:
+        out.append(str(idx))
+
+if len(out) == 0:
+    print(-1)
+else:
+    sys.stdout.write("\n".join(out))
+
 
 """
-5 6
-101010
-111111
-000001
-111111
-111111
+4 4 2 1
+1 2
+1 3
+2 3
+2 4
+
+4 3 2 1
+1 2
+1 3
+1 4
+
+4 4 1 1
+1 2
+1 3
+2 3
+2 4
+
 """
-
-
