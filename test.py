375d4adc8dff332d32681cef5cc1c48224424f6a
Wattamelon <seongon1010@gmail.com>
2026-02-23 17:33:48 +0900

    sql

--- test.py
+++ test.py
@@ -1,14 +1,23 @@
-from pandas.io.sas.sas_constants import dataset_length
+from collections import deque
 
-n = int(input())
-data = list(map(int,input().split()))
+def solution(food_times, k):
+    food_times_idx = []
+    for idx , nums in enumerate(food_times):
+        tmp = {}
+        tmp[idx+1] = nums
+        food_times_idx.append(tmp) #[{1:3},{2:1}] ....
+    queue = deque(food_times_idx)
+    print(queue)
+    while k:
+        while queue[0].values() == [0]:
+            queue.rotate(-1)
+        tmp = list[queue[0].keys()][0]
+        queue[0][tmp] -= 1
+        "list(queue[0].values())[0] -= 1"
+        queue.rotate(-1)
+        k -= 1
+    print(queue)
+    print(queue[0])
 
-data.sort()
 
-target = 1
-for x in data:
-    if target < x:
-        break
-    target += x
-
-print(target)
+solution([3, 1, 2],	5)
